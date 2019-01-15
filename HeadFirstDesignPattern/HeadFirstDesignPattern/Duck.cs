7cee9036b2b8a78754ac3e4cb3a6187510113cff
jjoylee <ekdnlt1995@gmail.com>
2019-01-15 23:17:39 +0900

    2019.01.15 chapter1 strategy pattern

---
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/Duck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPattern
+{
+    public abstract class Duck
+    {
+        // fly, quack 부분에서만 계속 변한다. 이부분만 인터페이스로 뺴서 캡슐화
+        public FlyBehavior flyBehavior;
+        public QuackBehavior quackBehavior;
+
+        public Duck() { }
+
+        // abstract와 virtual의 차이
+
+        // 1.abstract
+        // 사용하려면 해당 클래스도 abstract여야한다.  
+        // 속성이나 메소드는 본문을 정의할 수 없다. 
+        // public, internal, protected -> 상속을 해야하기 때문에 private 불가
+
+        // 2.virtual
+        // 정적 클래스를 제외한 모든 클래스에서 사용 가능하다.
+        // 속성이나 메서드는 본문을 정의할 수 있다.
+
+        public abstract void display();
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠");
+        }
+
+        public void setFlyBehavior(FlyBehavior fb)
+        {
+            flyBehavior = fb;
+        }
+
+        public void setQuackBehavior(QuackBehavior qb)
+        {
+            quackBehavior = qb;
+        }
+    }
+}
---
+++ HeadFirstDesignPattern/HeadFirstDesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPattern
+{
+    // Strategy Pattern
+
+    // 애플리케이션에서 달라지는 부분을 찾아내고, 달라지지 않는 부분으로부터 분리시킨다.
+    // -> 달라지는 부분을 찾아서 나머지 코드에 영향을 주지 않도록 '캡슐화'한다.
+    // 구현이 아닌 인터페이스에 맞춰서 프로그래밍한다.
+    // 상속보다는 구성을 활용한다.
+
+    // 알고리즘군을 정의하고 각각을 캡슐화하여 교환해서 사용할 수 있도록 한다.
+    // 알고리즘을 사용하는 클라이언트와는 독립적으로 알고리즘을 변경할 수 있다.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.performQuack();
+            mallard.performFly();
+
+            Duck model = new ModelDuck();
+            model.performFly();
+            model.setFlyBehavior(new FlyRocketPowered());
+            model.performFly();
+        }
+    }
+}
